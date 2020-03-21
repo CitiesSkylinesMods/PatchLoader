d61c5f9adbe9bbd94265253bb419d787ebafb61e
egi <daegi01@gmail.com>
2020-03-21 20:02:01 +0100

    codestyle

--- PatchLoaderMod/LoaderConfig.cs
+++ PatchLoaderMod/LoaderConfig.cs
@@ -6,12 +6,14 @@ using ColossalFramework;
 using ColossalFramework.Plugins;
 using PatchLoaderMod.Utils;
 
-namespace PatchLoaderMod {
-    public class LoaderConfig {
-        private const string FileName = "doorstop_config.ini";
-        private const string Header = "[UnityDoorstop]";
-        private const string StateKey = "enabled";
-        private const string TargetAssemblyKey = "targetAssembly";
+namespace PatchLoaderMod
+{
+    public class LoaderConfig
+    {
+        private const string _fileName = "doorstop_config.ini";
+        private const string _header = "[UnityDoorstop]";
+        private const string _stateKey = "enabled";
+        private const string _targetAssemblyKey = "targetAssembly";
         private readonly string _defaultTargetAssemblyPath;
 
         public bool Enabled { get; private set; }
@@ -48,18 +50,23 @@ namespace PatchLoaderMod {
             SaveConfig();
         }
 
-        public void SaveConfig() {
+        public void SaveConfig()
+        {
             Log.Info($"Saving Loader config, actual loader state: [{Enabled}]");
             string content = BuildConfig();
             Log._Debug($"Saving config: \n{content}");
-            try {
-                using (FileStream stream = File.Create(FileName)) {
+            try
+            {
+                using (FileStream stream = File.Create(_fileName))
+                {
                     byte[] byteContent = new UTF8Encoding(true).GetBytes(content);
                     stream.Write(byteContent, 0, byteContent.Length);
                 }
 
                 Log.Info("Loader config saved successfully!");
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Log.Error("Something went wrong while saving config \n" + e);
             }
         }
@@ -68,49 +75,61 @@ namespace PatchLoaderMod {
         {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder
-                .AppendLine(Header)
-                .Append(StateKey).Append("=").AppendLine(Enabled.ToString().ToLower())
-                .Append(TargetAssemblyKey).Append("=").Append(LoaderPath.IsNullOrWhiteSpace() ? _defaultTargetAssemblyPath : LoaderPath);
+                .AppendLine(_header)
+                .Append(_stateKey).Append("=").AppendLine(Enabled.ToString().ToLower())
+                .Append(_targetAssemblyKey).Append("=").Append(LoaderPath.IsNullOrWhiteSpace() ? _defaultTargetAssemblyPath : LoaderPath);
 
             return stringBuilder.ToString();
         }
 
-        private void ReadConfig() {
+        private void ReadConfig()
+        {
             Log.Info($"Reading Loader config");
-            if (File.Exists(FileName)) {
+            if (File.Exists(_fileName))
+            {
                 Log.Info("Loader config found! Parsing...");
-                string[] lines = File.ReadAllLines( FileName);
+                string[] lines = File.ReadAllLines(_fileName);
                 ParseConfig(lines);
                 UpdateLoaderPath();
-            } else {
+            }
+            else
+            {
                 Log.Info("Loader config not found!");
             }
         }
 
-        private void UpdateLoaderPath() {
-            if (!_defaultTargetAssemblyPath.Equals(LoaderPath)) {
+        private void UpdateLoaderPath()
+        {
+            if (!_defaultTargetAssemblyPath.Equals(LoaderPath))
+            {
                 LoaderPath = _defaultTargetAssemblyPath;
                 Log.Info("Updating Loader config with new path location");
                 SaveConfig();
             }
         }
 
-        private void ParseConfig(string[] lines) {
-            if (lines.Length == 3) {
+        private void ParseConfig(string[] lines)
+        {
+            if (lines.Length == 3)
+            {
                 string[] stateKeyValue = lines[1].Split('=');
-                if (StateKey.Equals(stateKeyValue[0]) && bool.TryParse(
+                if (_stateKey.Equals(stateKeyValue[0]) && bool.TryParse(
                         stateKeyValue[1],
-                        out bool isEnabled)) {
+                        out bool isEnabled))
+                {
                     Enabled = isEnabled;
                 }
 
                 string[] targetPathKeyValue = lines[2].Split('=');
-                if (TargetAssemblyKey.Equals(targetPathKeyValue[0])) {
+                if (_targetAssemblyKey.Equals(targetPathKeyValue[0]))
+                {
                     LoaderPath = targetPathKeyValue[1];
                 }
 
                 Log.Info($"Loader config parsing complete. Status: [{(Enabled ? "enabled" : "disabled")}] Loader assembly path [{LoaderPath}])");
-            } else {
+            }
+            else
+            {
                 Log.Error("Invalid Config!");
             }
         }
