d3eadbc1aceb89bfcc3c8acad247a26def30944c
krzychu124 <krzychu124@gmail.com>
2021-04-28 22:12:39 +0200

    Fix broken Steam overlay on MacOS and Linux

--- PatchLoaderMod/Doorstop/UnixConfigProperties.cs
+++ PatchLoaderMod/Doorstop/UnixConfigProperties.cs
@@ -3,15 +3,17 @@ using System;
 namespace PatchLoaderMod.Doorstop {
     public class UnixConfigProperties {
         
-        public UnixConfigProperties(string header, string enabledStateKey, string targetAssemblyKey, string gameExePath)
+        public UnixConfigProperties(string header, string preloadKey, string enabledStateKey, string targetAssemblyKey, string gameExePath)
         {
             Header = header ?? throw new ArgumentNullException(nameof(header));
+            PreloadKey = preloadKey ?? throw new ArgumentNullException(nameof(preloadKey));
             EnabledStateKey = enabledStateKey ?? throw new ArgumentNullException(nameof(enabledStateKey));
             TargetAssemblyKey = targetAssemblyKey ?? throw new ArgumentNullException(nameof(targetAssemblyKey));
             GameExePath = gameExePath ?? throw new ArgumentNullException(nameof(gameExePath));
         }
 
         public string Header { get; }
+        public string PreloadKey { get; }
         public string EnabledStateKey { get; }
         public string TargetAssemblyKey { get; }
         public string GameExePath { get; }
--- Utils/Config.cs
+++ Utils/Config.cs
@@ -2,13 +2,14 @@ namespace Utils
 {
     public class Config
     {
+        public int ConfigVersion { get; set; }
         public string WorkshopPath { get; set; }
         
         public bool UpgradeInProgress { get; set; }
 
 
         public static Config InitialValues() {
-            return new Config(){UpgradeInProgress = false, WorkshopPath = ""};
+            return new Config(){ConfigVersion = 1, UpgradeInProgress = false, WorkshopPath = ""};
         }
     }
 }
